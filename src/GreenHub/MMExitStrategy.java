aaef8becfc839c3336b5256568dcc36ce4fcc601
Marco Di Domenica <didomenicamarco@gmail.com>
2024-10-13 18:46:10 +0200

    fixino

--- src/GreenHub/MMExitStrategy.java
+++ src/GreenHub/MMExitStrategy.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 
 public class MMExitStrategy implements MainMenuStrategy {
     @Override
-    public void execute(UserInterface view, User user) {
+    public void execute(UserInterface ui, User user) throws IOException {
+        ui.getController().saveAll();
         System.exit(0);
     }
 }
